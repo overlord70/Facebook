8a198ec0318a2c26c49121530e3639c7a47a5bae
Sardor <149042720+overlord70@users.noreply.github.com>
2024-05-27 23:13:03 +0500

    Update details.js
    ....

--- js/details.js
+++ js/details.js
@@ -1,13 +1,19 @@
 
  const id =  location.search.split('=').at(-1)
+
  let span_1 = document.querySelector('.number')
+
  let span_2 = document.querySelector('.user_name')
+
  let p = document.querySelector('p')
+
  fetch('http://localhost:8080/users/' +id)
  .then(res => res.json())
  .then(res => {
     span_2.innerHTML = res.name
+  
     p.innerHTML = JSON.stringify(res, null, 2)
    
  })
+
  span_1.innerHTML = id
