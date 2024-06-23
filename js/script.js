d26685c44cd5de6d3855d5dfa81bcba990194829
Sardor <149042720+overlord70@users.noreply.github.com>
2024-06-23 19:48:19 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 import { reload } from "./ui.js"
+
 let tick = document.querySelector('.tick')
 let temp = document.querySelector('.ten')
+
 fetch('http://localhost:8080/users')
 .then(res => res.json())
 .then(res => {
+    
     reload(res, tick)
    
 })
